6569ee12ef11dfd8e09d640c3c99ac2e5b5e7594
MentalHazee <santosjulian1997@gmail.com>
2025-08-30 01:24:42 -0300

    Equals terminado

--- 2_equals_y_hashCode/TP4/src/main/java/Test/test.java
+++ 2_equals_y_hashCode/TP4/src/main/java/Test/test.java
@@ -1,4 +1,17 @@
 package Test;
 
+import Entities.Producto;
+
 public class test {
+    public static void main(String[] args) {
+        Producto zapatillas = new Producto(5665, "Nike", 75.00);
+        Producto zapatillas2 = new Producto(5665, "Nike", 75.00);
+
+        System.out.println("Zapatillas repetidas: " + zapatillas2.equals(zapatillas) );
+
+        Producto zapatillas3 = new Producto(5664, "Puma", 75.00);
+        Producto zapatillas4 = new Producto(5655, "Adidas", 75.00);
+
+        System.out.println("Zapatillas repetidas: " + zapatillas3.equals(zapatillas4));
+    }
 }
