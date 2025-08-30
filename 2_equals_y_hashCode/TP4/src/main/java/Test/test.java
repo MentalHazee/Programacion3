bf2fb5912dae8c0be99283482c47a92963147549
MentalHazee <santosjulian1997@gmail.com>
2025-08-30 01:38:11 -0300

    Terminado

--- 2_equals_y_hashCode/TP4/src/main/java/Test/test.java
+++ 2_equals_y_hashCode/TP4/src/main/java/Test/test.java
@@ -1,17 +1,25 @@
 package Test;
 
 import Entities.Producto;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
+
+        List<Producto> listaDeProductos = new ArrayList<>();
         Producto zapatillas = new Producto(5665, "Nike", 75.00);
-        Producto zapatillas2 = new Producto(5665, "Nike", 75.00);
+        Producto zapatillas2 = new Producto(56655, "Nike", 120.00);
+        Producto zapatillas3 = new Producto(5664, "Puma", 85.00);
+        Producto zapatillasDuplicadas = new Producto(5665, "Nike", 75.00);
 
-        System.out.println("Zapatillas repetidas: " + zapatillas2.equals(zapatillas) );
+        listaDeProductos.add(zapatillas);
+        listaDeProductos.add(zapatillas2);
+        listaDeProductos.add(zapatillas3);
 
-        Producto zapatillas3 = new Producto(5664, "Puma", 75.00);
-        Producto zapatillas4 = new Producto(5655, "Adidas", 75.00);
+        System.out.println(listaDeProductos.contains(zapatillasDuplicadas));
 
-        System.out.println("Zapatillas repetidas: " + zapatillas3.equals(zapatillas4));
+
+
     }
 }
