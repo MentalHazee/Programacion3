93d69f7025fe0bd7c2f132bfee9a6ab8868f8460
MentalHazee <santosjulian1997@gmail.com>
2025-08-31 19:10:06 -0300

    Terminado

---
+++ 4_Lombok_y_DTO/TP1/src/main/java/Entities/Persona.java
@@ -0,0 +1,16 @@
+package Entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+}
---
+++ 4_Lombok_y_DTO/TP1/src/main/java/Test/test.java
@@ -0,0 +1,18 @@
+package Test;
+
+import Entities.Persona;
+
+public class test {
+    public static void main(String[] args) {
+        Persona isaac = Persona.builder()
+                .nombre("Isaac Newton")
+                .edad(382)
+                .build();
+
+        Persona albert = Persona.builder()
+                .build();
+
+        System.out.println(isaac);
+        System.out.println(albert);
+    }
+}
