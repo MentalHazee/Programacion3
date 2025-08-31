eac84fc75ec2a0e272b569d2a9676d0161c9c0b6
MentalHazee <santosjulian1997@gmail.com>
2025-08-31 17:33:38 -0300

    Creación del proyecto

---
+++ 3_Colecciones/TP3/src/main/java/Test/test.java
@@ -0,0 +1,13 @@
+package Test;
+
+import Entities.Curso;
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+    public static void main(String[] args) {
+        Map<String, Curso> cursos = new HashMap<>();
+
+
+    }
+}
