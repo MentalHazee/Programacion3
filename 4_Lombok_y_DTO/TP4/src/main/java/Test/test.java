0349d2fced0ab79e7fa90bc6c132e8d214dea230
MentalHazee <santosjulian1997@gmail.com>
2025-09-11 21:07:49 -0300

    Terminado

--- 4_Lombok_y_DTO/TP4/src/main/java/Test/test.java
+++ 4_Lombok_y_DTO/TP4/src/main/java/Test/test.java
@@ -3,6 +3,9 @@ package Test;
 import DTOs.ProductoRecord;
 import Entities.Producto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
     public static void main(String[] args) {
         Producto memoriaRam = new Producto("444", "Memoria RAM", 54000.00);
@@ -12,5 +15,15 @@ public class test {
         ProductoRecord r1 = new ProductoRecord(memoriaRam.getCodigo(), memoriaRam.getNombre(), memoriaRam.getPrecio());
         ProductoRecord r2 = new ProductoRecord(placaDeVideo.getCodigo(), placaDeVideo.getNombre(), placaDeVideo.getPrecio());
         ProductoRecord r3 = new ProductoRecord(procesador.getCodigo(), procesador.getNombre(), procesador.getPrecio());
+
+        List<ProductoRecord> lista = new ArrayList<>();
+        lista.add(r1);
+        lista.add(r2);
+        lista.add(r3);
+
+        System.out.println("=== Lista de ProductosRecord ===");
+        for (ProductoRecord r : lista) {
+            System.out.println(r);
+        }
     }
 }
