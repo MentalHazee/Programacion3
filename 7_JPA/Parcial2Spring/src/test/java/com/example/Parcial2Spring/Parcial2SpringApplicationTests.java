20a61defb71f23ae17fed5bbbdfdca4f589e9bd7
MentalHazee <santosjulian1997@gmail.com>
2025-10-19 13:43:39 -0300

    Creación del parcial 2

---
+++ 7_JPA/Parcial2Spring/src/test/java/com/example/Parcial2Spring/Parcial2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Parcial2Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
